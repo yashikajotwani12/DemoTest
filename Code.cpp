e44ae4530f250dcb305344cd6a1f99df2d4bc24f
Yashika Jotwani <yashikajotwani12@gmail.com>
2023-04-05 12:37:05 +0530

    Update file

--- Code.cpp
+++ Code.cpp
@@ -1,51 +1,7 @@
- bool isMatch(string s, string p) {
-        vector<vector<bool>> dp(s.size()+1, vector<bool>(p.size()+1, false));
-        dp[0][0] = true;
-        
-        for(int j=1; j<=p.size(); j++)
-        {
-            if(p[j-1]=='*')
-                dp[0][j] = true;
-            else
-                break;
-        }
-        
-        for (int i=1; i<=s.size(); i++)
-        {
-            for (int j=1; j<=p.size(); j++)
-            {
-                if (s[i-1] == p[j-1] || p[j-1] == '?')
-                    dp[i][j] = dp[i-1][j-1];
-                else if (p[j-1] == '*')
-                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
-            }
-        }
-        
-        return dp.back().back();
-    }
+<p>&nbsp;tset //</p>
 
-bool isMatch(string s, string p) {
-        vector<vector<bool>> dp(s.size()+1, vector<bool>(p.size()+1, false));
-        dp[0][0] = true;
-        
-        for(int j=1; j<=p.size(); j++)
-        {
-            if(p[j-1]=='*')
-                dp[0][j] = true;
-            else
-                break;
-        }
-        
-        for (int i=1; i<=s.size(); i++)
-        {
-            for (int j=1; j<=p.size(); j++)
-            {
-                if (s[i-1] == p[j-1] || p[j-1] == '?')
-                    dp[i][j] = dp[i-1][j-1];
-                else if (p[j-1] == '*')
-                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
-            }
-        }
-        
-        return dp.back().back();
-    }
+<p>&nbsp;</p>
+
+<p>bool isMatch(string s, string p) { vector&gt; dp(s.size()+1, vector(p.size()+1, false)); dp[0][0] = true; for(int j=1; j&lt;=p.size(); j++) { if(p[j-1]==&#39;*&#39;) dp[0][j] = true; else break; } for (int i=1; i&lt;=s.size(); i++) { for (int j=1; j&lt;=p.size(); j++) { if (s[i-1] == p[j-1] || p[j-1] == &#39;?&#39;) dp[i][j] = dp[i-1][j-1]; else if (p[j-1] == &#39;*&#39;) dp[i][j] = dp[i][j-1] || dp[i-1][j]; } } return dp.back().back(); } bool isMatch(string s, string p) { vector&gt; dp(s.size()+1, vector(p.size()+1, false)); dp[0][0] = true; for(int j=1; j&lt;=p.size(); j++) { if(p[j-1]==&#39;*&#39;) dp[0][j] = true; else break; } for (int i=1; i&lt;=s.size(); i++) { for (int j=1; j&lt;=p.size(); j++) { if (s[i-1] == p[j-1] || p[j-1] == &#39;?&#39;) dp[i][j] = dp[i-1][j-1]; else if (p[j-1] == &#39;*&#39;) dp[i][j] = dp[i][j-1] || dp[i-1][j]; } } return dp.back().back(); }</p>
+
+<p>&nbsp;</p>
